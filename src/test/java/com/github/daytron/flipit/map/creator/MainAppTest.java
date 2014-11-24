8b542191a977c406917e31f9057c76e05e982d50
Daytron <ryangilera@gmail.com>
2014-11-24 15:21:09 +0000

    Add license header

--- src/test/java/com/github/daytron/flipit/map/creator/MainAppTest.java
+++ src/test/java/com/github/daytron/flipit/map/creator/MainAppTest.java
@@ -1,7 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * The MIT License
+ *
+ * Copyright 2014 Ryan Gilera ryangilera@gmail.com.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.github.daytron.flipit.map.creator;
 
@@ -19,6 +37,7 @@ import org.junit.AfterClass;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.loadui.testfx.Assertions;
 import org.loadui.testfx.controls.Commons;
@@ -28,7 +47,7 @@ import org.testfx.api.FxLifecycle;
 
 /**
  *
- * @author ryan
+ * @author Ryan Gilera
  */
 public class MainAppTest extends FxRobotImpl {
 
@@ -240,5 +259,21 @@ public class MainAppTest extends FxRobotImpl {
         assertTrue(this.app.getView().isThereAMapVisible());
         assertTrue(this.app.getView().isEditMapOn());
     }
+    
+    @Test
+    @Ignore
+    public void clickFileOpenMapTest() {
+        clickOn("#file").clickOn("#open");
+        sleep(1, SECONDS);
+        
+        //clickOn(".file-path-textfield").write("/home/user/fixtures/enterprise-data.xml"); 
+    }
 
+    @Test
+    public void clickHelpAboutAppTest() {
+        clickOn("#help").clickOn("#about");
+        sleep(1, SECONDS);
+        closeCurrentWindow();
+        sleep(1, SECONDS);
+    }
 }
